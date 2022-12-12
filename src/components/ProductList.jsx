97e6b5c56bb198abbf0085dd4a6195a597ea473d
Fahmi Efendy <ghokemons@gmail.com>
2022-12-12 14:11:29 +0700

    fix homepage product list

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 import { Box, Button, Grid, Typography, useMediaQuery } from "@mui/material";
@@ -8,11 +8,12 @@ import { Product } from "./";
 
 const ProductList = (props) => {
   const {
-    // fourItem = false,
+    eightItem = false,
     // recommendation = false,
     viewAllButton = false,
-    // productType,
-    // productPath,
+    productType,
+    productPath,
+    productCategory,
     // hideProduct,
   } = props;
 
@@ -26,76 +27,69 @@ const ProductList = (props) => {
   //   (data) => data.no !== hideProduct
   // );
 
-  // const products = (recommendationProduct) => {
-  //   if (fourItem) {
-  //     return recommendationProduct.slice(0, 4);
-  //   } else if (!fourItem) {
-  //     return recommendationProduct;
-  //   }
-  // };
+  const allProducts = productListDummy
+    .find((data) => data.productCategory === productCategory)
+    .data.flatMap((data) => data.data);
+
+  const formattedProducts = (data) => {
+    if (eightItem) {
+      return data.slice(0, 8);
+    } else if (!eightItem) {
+      return data;
+    }
+  };
 
   return (
     <>
-      {productListDummy.map((data, index) => {
-        const productCategory = data.productCategory;
-        return (
-          <Fragment key={index}>
-            <Box style={{ display: "flex", marginBottom: ".5rem" }}>
-              <Typography
-                variant={matches ? "body1" : "h5"}
-                style={{ fontWeight: "700", marginTop: "2.5rem" }}
-              >
-                {productCategory}
-              </Typography>
-              {viewAllButton && (
-                <Link
-                  to={`/${productCategory}`}
-                  style={{
-                    textDecoration: "none",
-                    color: "#000000",
-                    marginLeft: "auto",
-                    alignSelf: "flex-end",
-                  }}
-                >
-                  <Button
-                    size="small"
-                    style={{
-                      fontSize: matches ? "10px" : "12px",
-                      border: "1px solid #AF1515",
-                      height: matches ? "1.5rem" : "2rem",
-                      width: matches ? "3rem" : "5rem",
-                      color: "#AF1515",
-                      textTransform: "capitalize",
-                    }}
-                  >
-                    View All
-                  </Button>
-                </Link>
-              )}
-            </Box>
-            <Grid container spacing={2}>
-              {data.data.map((data) => {
-                const productType = data.productType;
-                const productPath = data.productPath;
-                return data.data.map((data) => {
-                  return (
-                    <Grid key={data.no} item md={3} xs={6}>
-                      <Product
-                        productId={data.no}
-                        productImage={data.productImage}
-                        productName={data.productName}
-                        productPrice={data.productPrice}
-                        productType={productType}
-                        productPath={productPath}
-                      />
-                    </Grid>
-                  );
-                });
-              })}
+      <Box style={{ display: "flex", marginBottom: ".5rem" }}>
+        <Typography
+          variant={matches ? "body1" : "h5"}
+          style={{ fontWeight: "700", marginTop: "4rem" }}
+        >
+          {productCategory}
+        </Typography>
+        {viewAllButton && (
+          <Link
+            to={`/${productCategory}`}
+            style={{
+              textDecoration: "none",
+              color: "#000000",
+              marginLeft: "auto",
+              alignSelf: "flex-end",
+            }}
+          >
+            <Button
+              size="small"
+              style={{
+                fontSize: matches ? "10px" : "12px",
+                border: "1px solid #AF1515",
+                height: matches ? "1.5rem" : "2rem",
+                width: matches ? "3rem" : "5rem",
+                color: "#AF1515",
+                textTransform: "capitalize",
+              }}
+            >
+              View All
+            </Button>
+          </Link>
+        )}
+      </Box>
+      <Grid container spacing={2}>
+        {formattedProducts(allProducts).map((data) => {
+          return (
+            <Grid key={data.no} item md={3} xs={6}>
+              <Product
+                productId={data.no}
+                productImage={data.productImage}
+                productName={data.productName}
+                productPrice={data.productPrice}
+                productType={productType}
+                productPath={productPath}
+              />
             </Grid>
-          </Fragment>
-        );
-      })}
+          );
+        })}
+      </Grid>
     </>
   );
 };
